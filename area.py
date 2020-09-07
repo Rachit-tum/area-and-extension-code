9e9264f53ac36a276d372507d848c483c7419edb
Rachit-tum <70895277+Rachit-tum@users.noreply.github.com>
2020-09-07 17:26:07 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,5 @@
+import math
+r=1.1
+a=math.pi*(r**2)
+print("radius of the circle is: ", r)
+print("area of the circle with radius"," ",r, "is" , a)
---
+++ extension.py
@@ -0,0 +1,5 @@
+dict={"py":"python","doc":"word file","pdf":"adobe file"}
+a=input("give a file name: ")
+b=input("give the file extension: ")
+print("Input filename is" +":"+ a+"."+b)
+print("the extension of the file is"+"  "+ dict[b])
